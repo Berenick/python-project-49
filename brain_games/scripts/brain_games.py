6a7605b111e408fd180fa63a2369f92b7d68ce47
Nickolay Berezhencev <nickolay200112@gmail.com>
2024-01-28 21:57:14 +1000

    step 3 import cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import sys
+import os
+DIR_NAME = os.path.dirname(__file__)
+DIR_NAME += '/../'
+print(DIR_NAME)
+sys.path.append(DIR_NAME)
+import cli
 def main():
     print('Welcome to the Brain Games!')
+    cli.welcome_user()
 if __name__ == '__main__':
     main()
