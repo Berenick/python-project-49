52037a1867c66773f998b1e0f68d9ac3da1148f6
Nickolay Berezhencev <nickolay200112@gmail.com>
2024-01-30 21:46:20 +1000

    step 4 add linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,8 +6,12 @@ DIR_NAME += '/../'
 print(DIR_NAME)
 sys.path.append(DIR_NAME)
 import cli
+
+
 def main():
     print('Welcome to the Brain Games!')
     cli.welcome_user()
+
+
 if __name__ == '__main__':
     main()
