ae901306d152c9f234dc3c556221f48fd527255d
Nickolay Berezhencev <nickolay200112@gmail.com>
2024-02-11 21:54:17 +1000

    add brain prime game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from .games import prime_help
+
+
+def main():
+    prime_help.prime_game()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/games/prime_help.py
@@ -0,0 +1,32 @@
+import game_log
+from . import random
+import math
+
+
+def number_is_prime(num = int):
+    if num == 1:
+        return 'no'
+    a = []
+    for i in range(2, num + 1):
+        a.append(i)
+    for i in a:
+        if i > math.sqrt(num):
+            return 'yes'
+        if num % i == 0:
+            return 'no'
+
+
+def prime_game():
+        name = game_log.say_hello()
+        print('Answer "yes" if given number is prime. Otherwise answer "no".')
+        result_correct_answer = 0
+        while result_correct_answer != 3:
+            question_num = random.randint(1, 5000)
+            correct_answer = number_is_prime(question_num)
+            if game_log.ask_get_check_correct_answer(question_num, correct_answer) is True:
+                result_correct_answer += 1
+            else:
+                result_correct_answer = 0
+        print(f'Congratulations, {name}')
+
+prime_game()
